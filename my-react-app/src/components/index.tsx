a81db300fb685c60b693031ab27f18b28b2b300d
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-03-28 09:24:43 -0300

    finished structure. Next step make the graph

--- my-react-app/src/components/index.tsx
+++ my-react-app/src/components/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import "./style.scss"
+import logo from "../assets/logo.svg"
 
 export const Home = () => {
   return (
     <section className='container'>
-      <div className='balance'>My Balance</div>
 
-      <div className='lastSevenDays'>
-        Spending - Last 7 days
+      <div className='balanceContainer'>
+        <span className='myBalance'>My Balance</span>
+        <span className='value'>R$900,00</span>
+        <img src={logo} alt="logo" />
       </div>
 
+      <div className='lastSevenDays'>
+
+        <h3 className='title'>Spending - Last 7 days</h3>
+        <div className='graphic'></div>
+
+        <div className='totalMonth'>
+          <div className='leftContainer'>
+            <span className='thisMonth'>Total this month</span>
+            <span className='value'>R$500,00</span>
+          </div>
+
+          <div className='rightContainer'>
+            <span className='percentage'>+2.4%</span>
+            <span className='montLast'>from last month</span>
+          </div>
+        </div>
+
+      </div>
 
     </section>
   )
